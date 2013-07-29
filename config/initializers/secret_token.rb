2dfe1bfb9aaec6cd95e5ded4acb4d6a60d4c20e5
Stefan Mahlitz <stefan.mahlitz@salzwerk.net>
2013-07-29 21:25:50 +0200

    init rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProLOG::Application.config.secret_key_base = 'd5a7e79daeca6433e4facae4c7a6962caad0284b5e274a816baf4895192b3b9f59809dddce2c474233e00389bd00f89af1ef9c06ca01641428b9668b230e708d'
